5f70fcf98eed4cc01b1d4a3deab796a59eb5485a
eprikhodko <eprikhodkodev@gmail.com>
2022-11-20 11:20:26 +0400

    create app header

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts"></script>
 
 <template>
-  <header>
-    <h1 class="font-bold text-title-1 text-letters-accent">header text</h1>
-  </header>
+  <div class="mx-auto max-w-960">
+    <header class="">
+      <nav>
+        <ul class="flex">
+          <li class="py-20 mr-32 font-semibold text-2 text-letters-primary">
+            Converter
+          </li>
+          <li class="py-20 mr-32 font-semibold text-2 text-letters-primary">
+            Prices
+          </li>
+        </ul>
+      </nav>
+      <span
+        class="absolute left-0 right-0 border-b border-solid border-surface-secondary-03"
+      ></span>
+    </header>
+  </div>
 </template>
