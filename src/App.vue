d88b457c5c86f03aecf535f61cf1ae60dddd7f70
eprikhodko <eprikhodkodev@gmail.com>
2022-11-20 13:16:30 +0400

    add first input

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,10 @@
     <header class="">
       <nav>
         <ul class="flex">
-          <li class="py-20 mr-32 font-semibold text-2 text-letters-primary">
+          <li class="mr-32 py-20 text-2 font-semibold text-letters-primary">
             Converter
           </li>
-          <li class="py-20 mr-32 font-semibold text-2 text-letters-primary">
+          <li class="mr-32 py-20 text-2 font-semibold text-letters-primary">
             Prices
           </li>
         </ul>
@@ -17,5 +17,23 @@
         class="absolute left-0 right-0 border-b border-solid border-surface-secondary-03"
       ></span>
     </header>
+
+    <main>
+      <article>
+        <h1 class="mt-64 text-title-1 font-semibold text-letters-primary">
+          Bitcoin to US Dollar
+        </h1>
+        <h2
+          class="mt-16 mb-32 text-title-2 font-medium text-letters-secondary-01"
+        >
+          1 BTC equals $16,607.04 USD
+        </h2>
+
+        <input
+          type="number"
+          class="rounded-4 border border-solid border-surface-secondary-03 p-16 pt-20 text-title-2 font-medium text-letters-secondary-01"
+        />
+      </article>
+    </main>
   </div>
 </template>
